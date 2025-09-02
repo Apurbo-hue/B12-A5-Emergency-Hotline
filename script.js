8f7bf55a2d97968b1e9f5e01e00d49f47ca0fe54
Apurbo-hue <labunna123@gmail.com>
2025-09-02 14:10:33 +0600

    count features added

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
+//logic of heart count
+const hearts = document.querySelectorAll('.heart-btn');
+for (const heart of hearts) {
+    heart.addEventListener('click', function () {
+        const heartCount = document.getElementById('heart-count')
+        const intHeartCount = parseInt(heartCount.innerText)
+        const newHeartCount = intHeartCount + 1;
+        heartCount.innerText = newHeartCount;
+    })
+}
 
+//logic of call button 
+const stars = document.querySelectorAll('.call-btn');
+for (const star of stars) {
+    star.addEventListener('click', function () {
+         
+       
+        const catchStarCount = document.getElementById('stars')
+        const intCatchStarCount = parseInt(catchStarCount.innerText)
 
+        if (intCatchStarCount < 20) {
+            alert("Not enough stars!");
+            return; 
+        }
+
+        const newStarCount = intCatchStarCount - 20;
+        catchStarCount.innerText = newStarCount;
+        alert('this is the number')//need to modify
+    }
+    )
+}
+//logic of copy count 
 const buttons = document.querySelectorAll('.copy-btn');
 for (const button of buttons) {
     button.addEventListener('click', function () {
